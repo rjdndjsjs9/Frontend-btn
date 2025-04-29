895a3e49bdcc263d789fdba4cc4fbf1d1f64fd4b
Mona Lisa <ajiguruhprasetyo@gmail.com>
2025-04-29 20:51:36 +0700

    add helper chain pharos

--- lib/wallet.ts
+++ lib/wallet.ts
@@ -1,8 +1,8 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { mainnet, sepolia } from 'wagmi/chains';
+import { pharos } from './chain';
 
 export const config = getDefaultConfig({
     appName: 'Be The Nation',
     projectId: 'YOUR_PROJECT_ID', // You can get this from https://cloud.walletconnect.com
-    chains: [mainnet, sepolia],
+    chains: [pharos],
 }); 
