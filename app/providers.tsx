2dd16763cf238763c06962e2e316f19cdfe5d30f
Mona Lisa <ajiguruhprasetyo@gmail.com>
2025-04-29 19:33:27 +0700

    connect wallet

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,22 @@
 "use client";
 
 import { ReactNode } from "react";
+import { WagmiConfig } from "wagmi";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { config } from "@/lib/wallet";
+import "@rainbow-me/rainbowkit/styles.css";
+
+const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <WagmiConfig config={config}>
+        <RainbowKitProvider>
+          {children}
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </QueryClientProvider>
+  );
 }
