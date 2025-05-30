f29918da0a0640e226f2c4511ba164ce61de457a
Danur <sunarniedisunandar>
2025-05-30 17:23:41 +0700

    Metrics-List

--- backend/bin/helper/idGenerator.js
+++ backend/bin/helper/idGenerator.js
@@ -1,9 +1,35 @@
-const crypto = require('crypto');
+const COUNTRY_REGIONS = {
+// Asian    
+    'ID': '1', 
+    'JP': '1',
+    'CN': '4', 
+    'KR': '4',
+    'IN': '4',
+
+// America
+    'US': '2', 
+    'CA': '2',
+    'MX': '2',
+    'BR': '6', 
+
+
+// Europe
+    'DE': '3',
+    'GB': '3',
+
+//Australia    
+    'AU': '5',
+
+//Russian    
+    'RU': '7'
+};
 
 function generateMetricId(countryCode) {
-    const timestamp = new Date().getTime();
-    const randomString = crypto.randomBytes(4).toString('hex');
-    return `${countryCode}-${timestamp}-${randomString}`;
+    // Get region number or default to '0' if not found
+    const regionNumber = COUNTRY_REGIONS[countryCode] || '0';
+    
+    // Combine country code with region number
+    return `${countryCode}${regionNumber}`;
 }
 
 module.exports = { generateMetricId };
